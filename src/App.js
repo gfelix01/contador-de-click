7edb1aa334300b8e69611cf22f2b451d4a2708cf
Gabriel Arturo Felix Paez <gabriel_arturo01@hotmail.com>
2023-12-13 10:58:55 -0400

    update 5 files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import  logo from './img/logo.jpg';
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='App'>
+     <div  className='Rocco-logo-contenedor'>
+     
+      <img className='Rocco-logo' src= {logo} alt='Rocco logo'/>
+     </div>
+     <div className='contenedor-principal'>
+      <h1>Bienvenido al contador de click</h1>
+     </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/componentes/Boton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Boton({texto}){
+    return(
+      <button>
+        class 
+     {texto}
+      </button>
+    )
+}
+export default Boton
